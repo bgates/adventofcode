7450f60d107385d31d6a79f13c4954cf83c6c2b0
Brian Gates <brian.gates@theironyard.com>
2017-12-09 00:48:03 -0500

    cleaner, no faster

--- 2017/day9.js
+++ 2017/day9.js
@@ -1,23 +1,20 @@
 const wrap = require('./wrap')
 
 const part1 = data => {
-  data = data.replace(/!./g,'')
-  data = data.replace(/<.*?>/g, '')
-  const chars = data.split('')
+  data = data.replace(/!./g,'').replace(/<.*?>/g, '')
   let outer = 0
-  let score = 0
-  chars.forEach(char => {
+  const score = data.split('').reduce((total, char) => {
     if (char === '{') {
       outer++
     } else if (char === '}') {
-      score += outer 
+      total += outer 
       outer--
     }
-  })
+    return total
+  }, 0)
   console.log(score)
 }
 
-/*wrap(9, part1)
 part1('<>')
 part1('<random characters>')
 part1('<<<<<>')
@@ -34,17 +31,13 @@ part1('{<a>,<a>,<a>,<a>}')
 part1('{{<ab>},{<ab>},{<ab>},{<ab>}}')
 part1('{{<!!>},{<!!>},{<!!>},{<!!>}}')
 part1('{{<a!>},{<a!>},{<a!>},{<ab>}}')
-//wrap(9, part1)*/
+wrap(9, part1)
 
 const part2 = data => {
-  const str = data
-  const orig = data.length
   data = data.replace(/!./g,'')
-  const withoutCancel = orig - data.length
-  const withoutBracket = data.replace(/<.*?>/g,'<>').length
-  data = data.replace(/<.*?>/g, '')
-  const final = data.length
-  console.log(str, orig - withoutBracket - withoutCancel)
+  const withoutCancel = data.length
+  const final = data.replace(/<.*?>/g,'<>').length
+  console.log(withoutCancel - final)
 }
 part2('<>')
 part2('<random characters>')
