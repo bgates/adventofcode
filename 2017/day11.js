029efd42cc8d260f5da829d0f02d601e2496004a
Brian Gates <brian.gates@theironyard.com>
2017-12-11 01:53:05 -0500

    cleaner

--- 2017/day11.js
+++ 2017/day11.js
@@ -23,14 +23,19 @@ const mutate = (obj, directions) => {
   })
 }
 
-const part1 = data => {
-  let steps = data.trim().split(',')
-
+const setup = (data, cb) => {
+  const steps = data.trim().split(',')
   const directions = 'n,ne,se,s,sw,nw'.split(',')
-  let obj = directions.reduce((o, dir) => Object.assign({}, o, { [dir]: steps.filter(s => s === dir).length }), {})
+  const obj = directions.reduce(cb(steps), {})
+  return { steps, directions, obj }
+}
+
+const propSum = obj => Object.values(obj).reduce((tot, n) => tot + n, 0)
+const part1 = data => {
+  const cb = steps => (o, dir) => Object.assign({}, o, { [dir]: steps.filter(s => s === dir).length })
+  let { steps, directions, obj } = setup(data, cb)
   mutate(obj, directions)
-  let ans = Object.assign({}, obj, { total: Object.values(obj).reduce((tot, n) => tot + n, 0) })
-  console.log(ans.total)
+  console.log(propSum(obj))
 }
 
 part1('ne,ne,ne')
@@ -41,17 +46,16 @@ part1('se,sw,se,sw,sw')
 wrap(11, part1)
 
 part2 = data => {
-  let steps = data.trim().split(',')
-  const directions = 'n,ne,se,s,sw,nw'.split(',')
-  const start = directions.reduce((obj, d) => Object.assign({}, obj, { [d]:0 }), {})
+  const cb = () => (obj, d) => Object.assign({}, obj, { [d]: 0 })
+  let { steps, directions, obj } = setup(data, cb)
   let max = 0
-  let final = steps.reduce((obj, s, i) => {
-    obj[s]++
-    mutate(obj, directions)
-    max = Math.max(max, Object.values(obj).reduce((tot, n) => tot + n, 0))
-    return obj
-  }, start)
+  let final = steps.reduce((acc, s, i) => {
+    acc[s]++
+    mutate(acc, directions)
+    max = Math.max(max, propSum(acc))
+    return acc
+  }, obj)
   console.log(max)
 }
-wrap(11, part2)
 
+wrap(11, part2)
